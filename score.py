e9e4eaf733998560d4f3fb463fde13c6784d9828
Tashirev <95381875+Tashirev@users.noreply.github.com>
2022-11-29 23:13:44 +0300

    Add files via upload

---
+++ score.py
@@ -0,0 +1,33 @@
+def calc_mrr(predict, answer):
+    for i in range(len(predict)):
+        if predict[i] == answer:
+            return 1. / (i + 1)
+    return 0
+
+max_prediction_len = 100
+
+def calc_score_path(target_path, predict_path):
+    with open(target_path) as f:
+        y_true = [int(x.strip()) for x in f.readlines()]
+
+    with open(predict_path) as f:
+        y_pred = [[int(x) for x in line.strip().split(' ')] for line in f.readlines()]
+
+    mrr_score = 0
+    for (pred, answer) in zip(y_pred, y_true):
+        if len(pred) > max_prediction_len:
+            raise ValueError('$maximum prediction length is {}, got {}$'.format(max_prediction_len, len(y_pred[i])))
+        mrr_score += calc_mrr(pred, answer)
+
+    print("MRR@100 = {}".format(mrr_score / len(y_true)))
+
+    
+def calc_score_list(y_true, y_pred):
+
+    mrr_score = 0
+    for (pred, answer) in zip(y_pred, y_true):
+        if len(pred) > max_prediction_len:
+            raise ValueError('$maximum prediction length is {}, got {}$'.format(max_prediction_len, len(y_pred[i])))
+        mrr_score += calc_mrr(pred, answer)
+
+    return mrr_score / len(y_true)
